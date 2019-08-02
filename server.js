af5cd536c11186971917719f0919b7b013a07027
Alvin Oktavianus <agentsam1995@gmail.com>
2019-08-02 14:55:34 +0700

    Add custom server created with express

---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const next = require('next');
+
+const serverPort = process.env.PORT || 3000;
+const dev = process.env.NODE_DEV !== 'production';
+const app = next({dev});
+const handle = app.getRequestHandler();
+
+app
+  .prepare()
+  .then(() => {
+    const server = express();
+    // server.use(bodyParser.json());
+    // server.use(bodyParser.urlencoded({extended: true}));
+
+    server.get('/user/:id', (req, res) => {
+      const actualPage = '/user';
+      const queryParams = {id: req.params.id};
+      app.render(req, res, actualPage, queryParams);
+    });
+
+    server.get('*', (req, res) => {
+      return handle(req, res);
+    });
+
+    server.listen(serverPort, err => {
+      if (err) throw err;
+      console.log(`Ready at http://localhost:${serverPort}`)
+    });
+  })
+  .catch((ex) => {
+    console.error(ex);
+    process.exit(1);
+  });
