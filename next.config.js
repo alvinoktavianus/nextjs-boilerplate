cb2d733e96576d0c87e804164f2307efa14705b9
Alvin Oktavianus <agentsam1995@gmail.com>
2019-08-12 13:45:33 +0700

    Fix vulnerable dependencies and configure css modules in sass

--- next.config.js
+++ next.config.js
@@ -1,26 +1,26 @@
-const withPlugins = require('next-compose-plugins');
-const withSass = require('@zeit/next-sass');
-const withBundleAnalyzer = require('@zeit/next-bundle-analyzer');
+const { PHASE_PRODUCTION_BUILD } = require('next-server/constants');
 const nextRuntimeDotenv = require('next-runtime-dotenv');
+const withPlugins = require('next-compose-plugins');
+
+const withSass = require('@zeit/next-sass')({
+  cssModules: true,
+  cssLoaderOptions: {
+    localIdentName: '[path]___[local]___[hash:base64:5]',
+  },
+  [PHASE_PRODUCTION_BUILD]: {
+    cssLoaderOptions: {
+      localIdentName: '[hash:base64:8]',
+    },
+  },
+});
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+});
 
 const withConfig = nextRuntimeDotenv({
   server: ['BACKEND_ENDPOINT', 'PORT'],
 });
 
 module.exports = withConfig(
-  withPlugins([[withSass], [withBundleAnalyzer]], {
-    analyzeServer: ['server', 'both'].includes(process.env.BUNDLE_ANALYZE),
-    analyzeBrowser: ['browser', 'both'].includes(process.env.BUNDLE_ANALYZE),
-    bundleAnalyzerConfig: {
-      server: {
-        analyzerMode: 'static',
-        reportFilename: '../bundles/server.html',
-      },
-      browser: {
-        analyzerMode: 'static',
-        reportFilename: '../bundles/client.html',
-      },
-    },
-    cssModules: true,
-  }),
+  withPlugins([[withSass], [withBundleAnalyzer]], {}),
 );
